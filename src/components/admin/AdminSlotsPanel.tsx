31d1b5eba8c3ee8aa2f6ceb66fea064de70eaea1
acid <scplatz55@gmail.com>
2026-03-20 23:28:23 +0300

    rework slots panel 2

--- src/components/admin/AdminSlotsPanel.tsx
+++ src/components/admin/AdminSlotsPanel.tsx
@@ -329,7 +329,7 @@ export default function AdminSlotsPanel({
                     )}
                   </div>
                 );
-              })
+              })}
           </div>
 
           <div
